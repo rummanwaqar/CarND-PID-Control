08ab19fd691835de674f5d5e88f22ba42fb7d0a0
rummanwaqar <rumman.waqar05@gmail.com>
2019-03-07 12:49:49 -0700

    base pid class structure

---
+++ src/pid.cpp
@@ -0,0 +1,26 @@
+#include "pid.hpp"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return 0.0;  // TODO: Add your total error calc here!
+}
